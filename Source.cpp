5313fda9a67f1e932a9c748871991fe4cb44ff61
Daineanu Denis <61796158+denisd17@users.noreply.github.com>
2020-04-22 19:58:00 +0300

    Update 19:58

--- Source.cpp
+++ Source.cpp
@@ -5,13 +5,134 @@
 #include "graf.h"
 using namespace std;
 
+void citire_n_vectori()
+{
+	int nr;
+	cout << "Introduceti numarul de obiecte: ";
+	cin >> nr;
+	Vector *obiecte;
+
+	try {
+		obiecte = new Vector[nr];
+	}
+	catch (bad_alloc&)
+	{
+		cout << "EROARE: Eroare in alocarea memoriei!";
+		cin.get();
+		exit(1);
+	}
+
+	for (int i = 0; i < nr; i++)
+		cin >> obiecte[i];
+
+	for (int i = 0; i < nr; i++)
+		cout << obiecte[i];
+
+}
+
+void citire_n_matrice()
+{
+	int nr;
+	cout << "Introduceti numarul de obiecte: ";
+	cin >> nr;
+	Matrice *obiecte;
+
+	try {
+		obiecte = new Matrice[nr];
+	}
+	catch (bad_alloc&)
+	{
+		cout << "EROARE: Eroare in alocarea memoriei!";
+		cin.get();
+		exit(1);
+	}
+
+	for (int i = 0; i < nr; i++)
+		cin >> obiecte[i];
+
+	for (int i = 0; i < nr; i++)
+		cout << obiecte[i];
+
+}
+
+void citire_n_liste()
+{
+	int nr;
+	cout << "Introduceti numarul de obiecte: ";
+	cin >> nr;
+	Lista *obiecte;
+
+	try {
+		obiecte = new Lista[nr];
+	}
+	catch (bad_alloc&)
+	{
+		cout << "EROARE: Eroare in alocarea memoriei!";
+		cin.get();
+		exit(1);
+	}
+
+	for (int i = 0; i < nr; i++)
+		cin >> obiecte[i];
+
+	for (int i = 0; i < nr; i++)
+		cout << obiecte[i];
+
+}
+
+void citire_n_grafuri_neorientate()
+{
+	int nr;
+	cout << "Introduceti numarul de obiecte: ";
+	cin >> nr;
+	Graf_Neorientat *obiecte;
+
+	try {
+		obiecte = new Graf_Neorientat[nr];
+	}
+	catch (bad_alloc&)
+	{
+		cout << "EROARE: Eroare in alocarea memoriei!";
+		cin.get();
+		exit(1);
+	}
+
+	for (int i = 0; i < nr; i++)
+		cin >> obiecte[i];
+
+	for (int i = 0; i < nr; i++)
+		cout << obiecte[i];
+
+}
+
+void citire_n_grafuri_orientate()
+{
+	int nr;
+	cout << "Introduceti numarul de obiecte: ";
+	cin >> nr;
+	Graf_Orientat *obiecte;
+
+	try {
+		obiecte = new Graf_Orientat[nr];
+	}
+	catch (bad_alloc&)
+	{
+		cout << "EROARE: Eroare in alocarea memoriei!";
+		cin.get();
+		exit(1);
+	}
+
+	for (int i = 0; i < nr; i++)
+		cin >> obiecte[i];
+
+	for (int i = 0; i < nr; i++)
+		cout << obiecte[i];
+
+}
+
 int main()
 {
-	Lista l(3);
-
-	l.citire_date();
-
-	cout << l;
+	citire_n_matrice();
 
 	
 
